1fc9ada4d45ab7e89e14141f4813a524a81c6108
magkuna <magkuna@gmail.com>
2019-03-31 11:35:34 +0200

    rendering tasks

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 class ToDo {
     constructor(selector) {
         this.container = document.querySelector(selector) || document.body
+       
         this.tasks = [{
+            text: "Lista zadań do zrobienia",
             isCompleted: false,
         }]
+
+        this.render()
+    }
+
+    render() {
+        this.container.innerHTML = ''
+
+       
+
+        this.tasks.forEach(
+            task =>this.renderTask(task)
+        )
+
+    }
+    renderTask(task){
+        const div = document.createElement ('div')
+
+        div.innerText = task.text
+
+        if (task.isCompleted) div.style.textDecoration = 'line-through'
+
+        this.container.appendChild(div)
+    
     }
 }
