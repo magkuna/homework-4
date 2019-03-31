9dde936e12a84481bf483a0182483b468cb7285a
magkuna <magkuna@gmail.com>
2019-03-31 12:07:06 +0200

    this.tasks redefined

--- script.js
+++ script.js
@@ -1,71 +1,85 @@
-class ToDo {
-    constructor(selector) {
-        this.container = document.querySelector(selector) || document.body
-       
-        this.tasks = [{
-            text: "Lista zadań do zrobienia",
-            isCompleted: false,
-        }]
 
-        this.render()
-    }
-    toggleTask(taskIndex){
-        this.tasks[taskIndex].isCompleted = !this.tasks[taskIndex].isCompleted
+    class ToDo {
+        constructor(selector) {
+            this.container = document.querySelector(selector) || document.body
 
-        this.render()
-    }
-    
-    addTask(newTaskText){
-        const newTask = {
+            this.tasks = []
 
-            text:newTaskText,
-            isCompleted:false,
+            this.render()
         }
 
-        this.tasks = this.tasks.concat(newTask)
+        toggleTask(taskIndex) {
+            this.tasks = this.tasks.map(
+                (task, index) => (
+                    index === taskIndex ?
+                        {
+                            text: task.text,
+                            isCompleted: !task.isCompleted,
+                        }
+                        :
+                        task
+                )
+            )
 
-        this.render()
+            this.render()
+        }
+
+        addTask(newTaskText) {
+            const newTask = {
+                text: newTaskText,
+                isCompleted: false,
+            }
+
+            this.tasks = this.tasks.concat(newTask)
+
+            this.render()
+        }
+
+        render() {
+            this.container.innerHTML = ''
+
+            this.renderForm()
+
+            this.tasks.forEach(
+                (task, index) => this.renderTask(task, index)
+            )
+        }
+
+        renderTask(task, index) {
+            const div = document.createElement('div')
+
+            div.innerText = task.text
+
+            if (task.isCompleted) div.style.textDecoration = 'line-through'
+
+            div.addEventListener(
+                'click',
+                () => this.toggleTask(index)
+            )
+
+            this.container.appendChild(div)
+        }
+
+        renderForm() {
+            const div = document.createElement('div')
+            const input = document.createElement('input')
+            const button = document.createElement('button')
+
+            input.setAttribute('placeholder', 'Nowe zadanie')
+            button.innerText = 'Dodaj!'
+
+            const clickHandler = () => this.addTask(input.value)
+
+            button.addEventListener(
+                'click',
+                clickHandler
+            )
+
+            // check closure here (in [[Scopes]] property)
+            console.dir(clickHandler)
+
+            div.appendChild(input)
+            div.appendChild(button)
+            this.container.appendChild(div)
+        }
     }
-    render() {
-        this.container.innerHTML = ''
-
-        this.renderForm()
-
-        this.tasks.forEach(
-            task =>this.renderTask(task)
-        )
-
-    }
-    renderTask(task){
-        const div = document.createElement ('div')
-
-        div.innerText = task.text
-
-        if (task.isCompleted) div.style.textDecoration = 'line-through'
-
-        this.container.appendChild(div)
-    
-    }
-
-    renderForm(){
-        const div = document.createElement('div')
-        const input = document.createElement('input')
-        const button = document.createElement ('button')
-
-        input.setAttribute('placeholder', 'Nowe zadanie')
-        button.innerText = 'Dodaj!'
-
-        const clickHandler = () => this.addTask (input.value)
-
-        button.addEventListener(
-            'click',
-            clickHandler
-        )
-        console.dir(clickHandler)
-
-        div.appendChild(input)
-        div.appendChild(button)
-        this.container.appendChild(div)
-
-    }
-}
